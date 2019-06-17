373905cfaaab0ef6ce27df1373657ae5f8863a14
araik-madaryan <47305274+araik-madaryan@users.noreply.github.com>
2019-06-17 14:10:35 +0200

    Delete conf.js

--- routes/conf.js
+++
@@ -1,8 +0,0 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-host :  'localhost',
-user :  'root',
-password :  'armenia33',
-database :  'red',
-});
-module.exports = connection;
